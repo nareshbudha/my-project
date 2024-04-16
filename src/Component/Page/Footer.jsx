21db82199ab8971512042169faddb18459b9c33d
naresh099 <nareshkumarbudha@gmail.com>
2024-04-16 13:29:08 +0545

    footer fix

--- src/Component/Page/Footer.jsx
+++ src/Component/Page/Footer.jsx
@@ -13,15 +13,15 @@ import logoImage from "../Image/logo.png";
 const Footer = () => {
   return (
     <div>
-      <div className="bg-[#F7F7F7] flex mobile:flex-col mobile:py-10 mobile:gap-10 tablet:flex-row tablet:gap-10 tablet:px-10  ">
-        <div className=" flex flex-col mobile:gap-8 mobile:items-center">
+      <div className="bg-[#F7F7F7] flex mobile:flex-col mobile:py-10 mobile:gap-10 tablet:flex-row tablet:gap-10 tablet:px-5  ">
+        <div className="flex flex-col mobile:gap-8 mobile:items-center tablet:items-start">
           <img className="mobile:w-32 mobile:h-16 " src={logoImage} alt="Logo" />
-          <p className="text-[#000000] mobile:text-sm mobile:mx-12 tablet:w-40 tablet:text-sm tablet:font-light laptop:w-80 desktop:w-96">
+          <p className="text-[#000000] mobile:text-sm mobile:mx-12 tablet:mx-1 tablet:w-36 tablet:font-light laptop:w-80 desktop:w-96">
             We SPELL INNOVATION say we’re young because we’re peopled by young,
             energetic, curious and committed minds who are excited to solve one
             stop automated solution for you.
           </p>
-          <div className="flex mobile:hidden tablet:block tablet:w-full">
+          <div className="mobile:hidden tablet:block tablet:w-full">
             <FaSquareInstagram className="text-[#5F2466] text-3xl" />
             <FaFacebook className="text-[#5F2466] text-3xl " />
             <FaLinkedin className="text-[#5F2466] text-3xl " />
@@ -29,8 +29,8 @@ const Footer = () => {
           </div>
 
         </div>
-        <div className="flex flex-col  mobile:gap-5  tablet:flex-row ">
-          <div className="flex flex-col  mobile:gap-1 mobile:items-center tablet:w-48 laptop:w-80 desktop:w-80 ">
+        <div className="flex flex-col mobile:gap-5 tablet:flex-row tablet:items-start tablet:justify-start tablet:gap-1 ">
+          <div className="flex flex-col  mobile:gap-1 mobile:items-center tablet:w-28 tablet:items-start laptop:w-80 desktop:w-80 ">
             <h2 className="text-2xl text-[#5F2466] tablet:text-xl tablet:font-bold "> Quick Links</h2>
             <p className="text-[#000000] text-base font-light hover:font-bold">
               About</p>
@@ -42,7 +42,7 @@ const Footer = () => {
               Privacy Policy{" "}
             </p>
           </div>
-          <div className="flex flex-col mobile:gap-1 mobile:items-center tablet:w-48 laptop:w-80 desktop:w-96 ">
+          <div className="flex flex-col mobile:gap-1 mobile:items-center tablet:w-36 tablet:items-start laptop:w-80 desktop:w-96 ">
             <h2 className="text-2xl text-[#5F2466]   tablet:text-xl tablet:font-bold "> Our Associate</h2>
             <p className="text-[#000000] text-base font-light hover:font-bold  ">Spell SMS</p>
             <p className="text-[#000000] text-base font-light hover:font-bold">Spell Voice</p>
@@ -53,7 +53,7 @@ const Footer = () => {
               Spell Hosting
             </p>
           </div>
-          <div className="flex flex-col  mobile:gap-1 mobile:items-center tablet:text-sm laptop:w-80 desktop:w-96">
+          <div className="flex flex-col  mobile:gap-1 mobile:items-center tablet:text-sm tablet:w-40 tablet:items-start laptop:w-80 desktop:w-96">
             <h2 className="text-2xl text-[#5F2466]  tablet:text-xl tablet:font-bold "> Contact Us</h2>
             <div className="relative">
               <p className="text-[#000000] text-base hover:font-bold font-light ">
